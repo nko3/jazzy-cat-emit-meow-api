d61b6d48aa53cea073377c1ae7d8577f6f5bbed4
Zeus <lalkaka.zeus@gmail.com>
2012-11-10 12:12:52 -0800

    add test after hooks

--- models/index.js
+++ models/index.js
@@ -2,12 +2,20 @@
 var resourceful = require('resourceful'),
     cqs = require('../cqs');
 
+// TODO: Use the database var once we're done main development
+//var database = (process.env.NODE_ENV === 'production') ? 'prod' : 'testdb';
+
 resourceful.use('couchdb',
   'couchdb://nodejitsudb7300738890.iriscouch.com:5984/testdb');
 
 var Map = resourceful.define('map', function() {
 
   this.string('name');
+
+  this.after('save', function() {
+    console.log("After save arguments ", arguments);
+  });
+
 
 });
 
@@ -19,6 +27,11 @@ var Contribution = resourceful.define('contribution', function() {
   this.object('meta');
 
   this.parent('map');
+
+  this.after('save', function() {
+    console.log("After save arguments ", arguments);
+  });
+
 });
 
 
