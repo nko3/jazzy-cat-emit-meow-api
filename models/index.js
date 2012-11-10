3e72026406a466d159640bbc6f96500b853bfc58
Zeus <lalkaka.zeus@gmail.com>
2012-11-09 23:41:40 -0800

    Fix db

--- models/index.js
+++ models/index.js
@@ -1,12 +1,10 @@
 var resourceful = require('resourceful');
 
-resourceful.use('couchdb', {database: 'testdb'});
+resourceful.use('couchdb', 'couchdb://nodejitsudb7300738890.iriscouch.com:5984/testdb');
 
 // @Henry: See here for test cases:
 // https://github.com/flatiron/resourceful#api
 var Map = resourceful.define('map', function() {
-
-  this.use('couchdb', 'couchdb://nodejitsudb7300738890.iriscouch.com:5984/testdb');
 
   this.string('name');
 
@@ -14,8 +12,6 @@ var Map = resourceful.define('map', function() {
 
 
 var Contribution = resourceful.define('contribution', function() {
-
-  this.use('couchdb', 'couchdb://nodejitsudb7300738890.iriscouch.com:5984/testdb');
 
   this.string('lat');
   this.string('long');
