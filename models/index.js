f0be72d00bfdd0fe29745f411af3c05bf6ddcaab
Zeus <lalkaka.zeus@gmail.com>
2012-11-09 22:50:17 -0800

    Resourceful models.. so much cleaner

--- models/index.js
+++ models/index.js
@@ -1,102 +1,28 @@
-var redis = require('../redisClient');
+var resourceful = require('resourceful');
 
-var persistance = {
 
-  create: function(namespace, data, cb) {
-    // get the next id
-    // create the object
-    // add it to the namespace list
-    // publish a create event
-    // return the object
-  },
+var map = resourceful.define('map', function() {
 
-  retrieve: function(namespace, data, cb) {
-    // get the object
-    // return it
-  },
+  this.use('couchdb');
 
-  update: function(namespace, data, cb) {
-    // update
-    // publish update event
-  },
+  this.string('name');
 
-  del: function(namespace, data, cb) {
-    // delete
-    // publish delete event
-    // remove it from the namespace list
-  }
+});
+
+
+var contribution = resourceful.define('contribution', function() {
+
+  this.use('couchdb');
+
+  this.string('lat');
+  this.string('long');
+  this.object('meta');
+
+  this.parent('map');
+});
+
+
+module.exports = {
+  map: map,
+  contribution: contribution
 };
-
-
-var node = {
-  // Returns an array of errors or false if a valid node object was passed
-  invalid: function(data) {
-    var errors = [];
-    if (!data.lat) {
-      errors.push("A map node must have a lat property. Found " + data.lat || "undefined" + " instead");
-    }
-    if (!data.long) {
-      errors.push("A map node must have a long property. Found " + data.long || "undefined" + " instead");
-    }
-
-    if (errors.length) {
-      return errors;
-    }
-    else {
-      return false;
-    }
-  },
-
-  create: function(data, cb) {
-    var errors = this.invalid(data);
-    if (errors) {
-      return cb(errors);
-    }
-
-  },
-
-  retrieve: function(data, cb) {
-
-
-  },
-
-  update: function(data, cb) {
-
-
-  },
-
-  del: function(data, cb) {
-
-
-  }
-};
-
-var map = {
-
-  validate: function(data) {
-
-
-  },
-
-  create: function(data, cb) {
-
-  },
-
-  retrieve: function(data, cb) {
-
-  },
-
-  update: function(data, cb) {
-
-
-  },
-
-  del: function(data, cb) {
-
-
-  }
-};
-
-exports.map = map;
-exports.node = node;
-
