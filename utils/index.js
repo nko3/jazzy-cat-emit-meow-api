27f448e54302444756f3321f8326d0f4295c6547
Zeus <lalkaka.zeus@gmail.com>
2012-11-11 10:50:51 -0800

    start of teleportd migration util

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,24 @@
+var api = require('teleportd');
 
 
+api = api.teleportd({
+  user_key: '027cc743141ca9dc361a56d31bc912a9',
+  host: 'api.v2.teleportd.com'
+});
+
+console.log(api);
+
+var queries = [];
+var query = {
+  str: 'hurricane sandy',
+  size: 100,
+  from: 0
+};
+
+queries.push(query);
+
+queries.forEach(function(query) {
+  api.search(query, function(err, hits, total, took) {
+    console.log("Search returned", arguments);
+  });
+});
